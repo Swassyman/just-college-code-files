d3efbe8a5a9677c361ad9c5ba1a42a47fcd3bddd
Swassyman <swassyman2005@gmail.com>
2024-09-19 20:12:53 +0530

    reformatted code and fixed circular ll

--- DS Lab/Cycle-2/circularLL/circList.c
+++ DS Lab/Cycle-2/circularLL/circList.c
@@ -1,105 +1,227 @@
 #include<stdio.h>
 #include<stdlib.h>
-typedef struct Node{
-	int data;
-	struct Node* next;
-}Node;
-void insertFront(Node** head,Node** last, int x){
-	Node* newNode = (Node*)malloc(sizeof(Node));
-	if(newNode == NULL){
-		printf("Memory could not be allocated\n");
-		return;
-	}
-	newNode->data = x;
-	if(*head==NULL){
-		newNode->next = newNode;
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+void insertFront(Node** head, Node** last, int x) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Memory could not be allocated\n");
+        return;
+    }
+    newNode->data = x;
+    if (*head == NULL) {
+        newNode->next = newNode; // Point to itself for circular structure
         *head = newNode;
         *last = newNode;
-	}
-	else{
-		newNode->next = *head;
-		*head = newNode;
-		(*last)->next = *head;
-	}
+    } else {
+        newNode->next = *head;
+        *head = newNode;
+        (*last)->next = *head; // Make it circular
+    }
 }
-void insert(Node** head, Node** last, int pos, int x){
-	Node* temp = *head;
-	Node* newNode = (Node*)malloc(sizeof(Node));
-	if(newNode == NULL){
-		printf("Memory could not be allocated\n");
-		return;
-	}
-	newNode->data = x;
-	if(pos==1){
-		insertFront(head, last, x);
-		return;
-	}	
-	for(int i = 1; i<pos-1; i++){
-		temp = temp->next;
-	}
-	newNode->next = temp->next;
-	temp->next = newNode;
-	if(newNode->next==NULL){
-		*last = newNode;
-	}
+
+void insert(Node** head, Node** last, int pos, int x) {
+    Node* temp = *head;
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Memory could not be allocated\n");
+        return;
+    }
+    newNode->data = x;
+    if (pos == 1) {
+        insertFront(head, last, x);
+        return;
+    }
+
+    for (int i = 1; i < pos - 1; i++) {
+        temp = temp->next;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+
+    // If newNode is inserted at the end, update the last pointer
+    if (temp == *last) {
+        *last = newNode;
+        (*last)->next = *head; // Maintain circular structure
+    }
 }
-void display(Node* head){
-	if(head == NULL){
-		printf("List is Empty\n");
-		return;
-	}
-	Node* temp;
-	temp = head;
-	while(temp!=NULL){
-		if(temp->next==NULL)
-			printf("%d", temp->data);
-		else
-			printf("%d->", temp->data);
-		temp = temp->next;
-	}
-	printf("\n");
+
+void insertEnd(Node** head, Node** last, int x) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Memory could not be allocated\n");
+        return;
+    }
+    newNode->data = x;
+
+    if (*head == NULL) {
+        newNode->next = newNode; // Point to itself for circular structure
+        *head = newNode;
+        *last = newNode;
+    } else {
+        newNode->next = *head;
+        (*last)->next = newNode;
+        *last = newNode; // Update last pointer
+    }
 }
-int main(){
-	Node* head = NULL, * newNode;
-	Node* last->next = head;
-	int x;
-	printf("Enter data of first node");
-	scanf("%d", &x);
-	newNode = (Node*)malloc(sizeof(Node));
-	if(newNode == NULL){
-		printf("Memory could not be allocated\n");
-		return 0;
-	}
-	newNode->data = x;
-	newNode->next = last;
-	menu:
-	int choice;
-	printf("1-Insertion\n2-Deletion\n3-Display\n4-Exit\n");
-	scanf("%d", &choice);
-	switch(choice){
-		case 1:
-			int insertionChoice;
-			printf("1-At front\n2-Anywhere\n3-At end\n");
-			scanf("%d", &insertionChoice);
-			int item;
-			printf("Enter the data: ");
-			scanf("%d", &item);
-			switch(insertionChoice){
-				case 1:
-					insertFront(&head, &last, item);
-					goto menu;
-				case 2:
-					int position;
-					printf("At what position: ");
-					scanf("%d", &position);
-					if(position<=0){
-						printf("Invalid Position");
-						goto menu;
-					}
-					insert(&head, &last, position, item);
-					goto menu;
-			}
-	}
-	
-	return 0;
+
+void deleteFront(Node** head, Node** last) {
+    if (*head == NULL) {
+        printf("List is Empty\n");
+        return;
+    }
+
+    Node* temp = *head;
+    if (*head == *last) { // Only one element in the list
+        *head = NULL;
+        *last = NULL;
+    } else {
+        *head = (*head)->next;
+        (*last)->next = *head; // Maintain circular structure
+    }
+    free(temp);
+}
+
+void deletePosition(Node** head, Node** last, int pos) {
+    Node* temp = *head;
+    if (pos == 1) {
+        deleteFront(head, last);
+        return;
+    }
+
+    for (int i = 1; i < pos - 1; i++) {
+        temp = temp->next;
+    }
+
+    Node* temp2 = temp->next;
+    temp->next = temp->next->next;
+
+    // If the last node is deleted, update the last pointer
+    if (temp2 == *last) {
+        *last = temp;
+        (*last)->next = *head; // Maintain circular structure
+    }
+
+    free(temp2);
+}
+
+void deleteEnd(Node** head, Node** last) {
+    if (*head == NULL) {
+        printf("List is Empty\n");
+        return;
+    }
+
+    Node* temp = *head;
+    if (*head == *last) { // Only one element
+        *head = NULL;
+        *last = NULL;
+    } else {
+        while (temp->next != *last) {
+            temp = temp->next;
+        }
+        temp->next = *head;
+        free(*last);
+        *last = temp; // Update last pointer
+    }
+}
+
+void display(Node* head) {
+    if (head == NULL) {
+        printf("List is Empty\n");
+        return;
+    }
+
+    Node* temp = head;
+    do {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    } while (temp != head); // Stop when it loops back to head
+
+    printf("(back to head)\n");
+}
+
+int main() {
+    Node* head = NULL, *last = NULL;
+    int x;
+
+    menu:
+    {
+        int choice;
+        printf("1-Insertion\n2-Deletion\n3-Display\n4-Exit\n");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1: {
+                int insertionChoice;
+                printf("1-At front\n2-Anywhere\n3-At end\n");
+                scanf("%d", &insertionChoice);
+                int item;
+                printf("Enter the data: ");
+                scanf("%d", &item);
+
+                switch (insertionChoice) {
+                    case 1:
+                        insertFront(&head, &last, item);
+                        goto menu;
+                    case 2: {
+                        int position;
+                        printf("At what position: ");
+                        scanf("%d", &position);
+                        if (position <= 0) {
+                            printf("Invalid Position\n");
+                            goto menu;
+                        }
+                        insert(&head, &last, position, item);
+                        goto menu;
+                    }
+                    case 3:
+                        insertEnd(&head, &last, item);
+                        goto menu;
+                }
+            }
+
+            case 2: {
+                int deletionChoice;
+                printf("1-At front\n2-Anywhere\n3-At end\n");
+                scanf("%d", &deletionChoice);
+
+                switch (deletionChoice) {
+                    case 1:
+                        deleteFront(&head, &last);
+                        goto menu;
+                    case 2: {
+                        int position;
+                        printf("At what position: ");
+                        scanf("%d", &position);
+                        if (position <= 0) {
+                            printf("Invalid Position\n");
+                            goto menu;
+                        }
+                        deletePosition(&head, &last, position);
+                        goto menu;
+                    }
+                    case 3:
+                        deleteEnd(&head, &last);
+                        goto menu;
+                }
+            }
+
+            case 3:
+                display(head);
+                goto menu;
+
+            case 4:
+                return 0;
+
+            default:
+                goto menu;
+        }
+    }
+
+    return 0;
 }
--- DS Lab/Cycle-2/musicplaylist/music.c
+++ DS Lab/Cycle-2/musicplaylist/music.c
@@ -1,135 +1,152 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-typedef struct Node{
-	char musicName[50];
-	int durationSeconds;
-	char musicArtist[20];
-	struct Node* next;
-}Node;
-Node* createNode() {
-	Node* newNode = (Node*)malloc(sizeof(Node));
-	if( newNode == NULL ) {
-		printf("Memory could not be allocated\n");
-		return NULL;
-	}
-	return newNode;
-}
-void insertFront(Node** head,Node** last, char name[], int duration, char artist[]) {
-	Node* newNode = createNode();
-	
-	strcpy(newNode -> musicName, name);
-	strcpy(newNode -> musicArtist, artist);
-	newNode -> durationSeconds = duration;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-	if( *head == NULL ) {
-		newNode -> next = newNode;
+typedef struct Node {
+    char musicName[50];
+    int durationSeconds;
+    char musicArtist[20];
+    struct Node* next;
+} Node;
+
+Node* createNode() {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Memory could not be allocated\n");
+        return NULL;
+    }
+    return newNode;
+}
+
+void insertFront(Node** head, Node** last, char name[], int duration, char artist[]) {
+    Node* newNode = createNode();
+    if (newNode == NULL) return;
+
+    strcpy(newNode->musicName, name);
+    strcpy(newNode->musicArtist, artist);
+    newNode->durationSeconds = duration;
+
+    if (*head == NULL) {
+        newNode->next = newNode;
         *head = newNode;
         *last = newNode;
-	}
-	else {
-		newNode -> next = *head;
-		*head = newNode;
-		(*last) -> next = *head;
-	}
+    } else {
+        newNode->next = *head;
+        *head = newNode;
+        (*last)->next = *head;
+    }
 }
-void insert( Node** head, Node** last, int pos, char name[], int duration, char artist[]) {
-	Node* temp = *head;
-	Node* newNode = createNode();
 
-	strcpy(newNode -> musicName, name);
-	strcpy(newNode -> musicArtist, artist);
-	newNode -> durationSeconds = duration;
+void insert(Node** head, Node** last, int pos, char name[], int duration, char artist[]) {
+    if (pos == 1) {
+        insertFront(head, last, name, duration, artist);
+        return;
+    }
 
-	if( pos == 1 ) {
-		insertFront(head, last, name, duration, artist);
-		return;
-	}	
-	for( int i = 1; i < pos - 1 ; i++ ) {
-		temp = temp -> next;
-	}
-	newNode -> next = temp -> next;
-	temp -> next = newNode;
-	if( newNode -> next == NULL ) {
-		*last = newNode;
-	}
+    Node* temp = *head;
+    Node* newNode = createNode();
+    if (newNode == NULL) return;
+
+    strcpy(newNode->musicName, name);
+    strcpy(newNode->musicArtist, artist);
+    newNode->durationSeconds = duration;
+
+    for (int i = 1; i < pos - 1; i++) {
+        temp = temp->next;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+
+    if (newNode->next == NULL) {
+        *last = newNode;
+    }
 }
-void display( Node* head, int times) {
-	if( head == NULL ) {
-		printf("List is Empty\n");
-		return;
-	}
-	Node* temp;
-	temp = head;
-	int count = 0;
-	do {
+
+void display(Node* head, int times) {
+    if (head == NULL) {
+        printf("List is Empty\n");
+        return;
+    }
+
+    Node* temp = head;
+    int count = 0;
+    do {
         printf("Playing %s ~ %s : %d seconds\n", temp->musicName, temp->musicArtist, temp->durationSeconds);
         temp = temp->next;
-        if( temp == head) {
-        	count++;
-        	printf("-----------------------------------\n");
+        if (temp == head) {
+            count++;
+            printf("-----------------------------------\n");
         }
     } while (count < times);
-	printf("\n");
+    printf("\n");
 }
-int main(){
-	Node* head = NULL, * newNode = NULL;
-	Node* last = NULL;
-	
-	newNode = createNode();
 
-	int duration;
-	char name[50], artist[20];
+int main() {
+    Node* head = NULL;
+    Node* last = NULL;
 
-	menu:
-	int choice;
-	printf("1-Insert a Song\n2-Delete a Song\n3-Play\n4-Exit\n");
-	scanf("%d", &choice);
-	switch(choice){
-		case 1:
-			int insertionChoice;
-			printf("1-At front\n2-Anywhere\n3-At end\n");
-			scanf("%d", &insertionChoice);
+    int duration;
+    char name[50], artist[20];
 
-			printf("Enter name of the music: ");
-			scanf(" %[^\n]s", name);
+    while (1) {
+        int choice;
+        printf("1-Insert a Song\n2-Delete a Song\n3-Play\n4-Exit\n");
+        scanf("%d", &choice);
 
-			printf("Enter name of the artist: ");
-			scanf(" %[^\n]s", artist);
+        switch (choice) {
+            case 1: {
+                int insertionChoice;
+                printf("1-At front\n2-Anywhere\n3-At end\n");
+                scanf("%d", &insertionChoice);
 
-			printf("Enter duration in seconds: ");
-			scanf("%d", &duration);
+                printf("Enter name of the music: ");
+                scanf(" %[^\n]s", name);
 
-			switch(insertionChoice){
-				case 1:
-					insertFront(&head, &last, name, duration, artist);
-					goto menu;
-				case 2:
-					int position;
-					printf("At what position: ");
-					scanf("%d", &position);
-					if(position<=0){
-						printf("Invalid Position");
-						goto menu;
-					}
-					insert(&head, &last, position, name, duration, artist);
-					goto menu;
-			}
-		case 3:
-			int loop = 0;
-			printf("Do you want to keep looping?\n1- Yes\n0- No\n");
-			scanf("%d", &loop);
-			if(loop) {
-				printf("Enter number of times to loop: ");
-				int times;
-				scanf("%d", &times);
-				display( head, times);
-			}
-			else{
-				display(head, 1);
-			}
-			goto menu;
-	}
-	
-	return 0;
+                printf("Enter name of the artist: ");
+                scanf(" %[^\n]s", artist);
+
+                printf("Enter duration in seconds: ");
+                scanf("%d", &duration);
+
+                if (insertionChoice == 1) {
+                    insertFront(&head, &last, name, duration, artist);
+                } else if (insertionChoice == 2) {
+                    int position;
+                    printf("At what position: ");
+                    scanf("%d", &position);
+
+                    if (position <= 0) {
+                        printf("Invalid Position\n");
+                    } else {
+                        insert(&head, &last, position, name, duration, artist);
+                    }
+                }
+                break;
+            }
+
+            case 3: {
+                int loop;
+                printf("Do you want to keep looping?\n1- Yes\n0- No\n");
+                scanf("%d", &loop);
+
+                if (loop) {
+                    int times;
+                    printf("Enter number of times to loop: ");
+                    scanf("%d", &times);
+                    display(head, times);
+                } else {
+                    display(head, 1);
+                }
+                break;
+            }
+
+            case 4:
+                return 0;
+
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+    return 0;
 }
