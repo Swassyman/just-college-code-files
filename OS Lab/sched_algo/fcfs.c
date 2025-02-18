f72ebb6a51e60be0424715aea466b49eb898f986
Swassyman <swassyman2005@gmail.com>
2025-02-18 22:38:34 +0530

    fcfs done

---
+++ OS Lab/sched_algo/fcfs.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+void findWaitingTime(int processes[], int burst[], int n, int wt[]) {
+    wt[0] = 0;
+    for(int i = 1; i<n; i++) {
+        wt[i] = burst[i-1] + wt[i-1];
+    }
+}
+void findTurnAroundTime(int processes[], int burst[], int n, int wt[], int tat[]) {
+    for(int i = 0; i<n; i++) {
+        tat[i] = burst[i] + wt[i];
+    }
+}
+void print(int arr[], int n) {
+    for(int i = 0; i<n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+void averageTime(int processes[], int burst[], int n) {
+    int wt[n], tat[n];
+
+    //tofindwaitingtme
+    findWaitingTime(processes, burst, n, wt);
+    //sum it up and divide by n
+
+    //tofindturnaroundtime
+    findTurnAroundTime(processes, burst, n, wt, tat);
+    //sum it up and divide by n
+
+    print(wt, n);
+    print(tat, n);
+}
+int main() {
+    //array of processes
+    int processes[] = {1, 2, 3};
+    int n = sizeof(processes)/sizeof(processes[0]);
+
+    //array of burst times
+    int burst[] = {5, 10, 15};
+
+    //find average time of processes
+    averageTime(processes, burst, n);
+}
