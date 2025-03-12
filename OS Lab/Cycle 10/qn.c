6328cb90c56ae4385827465776ca8567a68590bc
Swassyman <swassyman2005@gmail.com>
2025-03-12 18:05:14 +0530

    improved code quality of banker's algo

--- OS Lab/Cycle 10/qn.c
+++ OS Lab/Cycle 10/qn.c
@@ -1,95 +1,92 @@
-#include<stdio.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-bool isSafe(int P, int R, int alloc[][R], int max[][10], int avail[]) {
+bool isSafe(int P, int R, int alloc[][R], int max[][R], int avail[]) {
+    int work[R];
+    bool finish[P];
+    int safeSeq[P];
+    int count = 0;
 
-	int work[P];
-	for(int i = 0; i<P; i++) {
-		work[i] = avail[i];	
-	}
+    for (int i = 0; i < R; i++) {
+        work[i] = avail[i];
+    }
 
-	bool finish[P] = {false};
-	int safeSeq[P];
-	int count = 0;
+    for (int p = 0; p < P; p++) {
+        finish[p] = false;
+    }
 
-	/*
-		Iterating through process table until we find a process that can be allocated
-		If 0 processes can be allocated at any iteration then system is not in safe state
-	*/
+    while (count < P) {
+        bool found = false;
+        for (int p = 0; p < P; p++) {
+            if (!finish[p]) {
+                bool canAllocate = true;
+                for (int r = 0; r < R; r++) {
+                    if (max[p][r] - alloc[p][r] > work[r]) {
+                        canAllocate = false;
+                        break;
+                    }
+                }
 
-	while(count < P) { //checking process list again and again
-		bool found = false; //checking if any process can be allocated in any iteration
-		for(int p = 0; p<P; p++) {
-			if(!finish[p]) { //if process did not finish
-				bool canAllocate = true; //if process can be allocated
-				for(int r = 0; r<R; r++) {
-					if(max[p][r] - alloc[p][r] > work[r]]) {
-						canAllocate = false;
-						break; //leave entire process if any resouce does not satisfy need
-					}
-				}
+                if (canAllocate) {
+                    for (int r = 0; r < R; r++) {
+                        work[r] += alloc[p][r];
+                    }
+                    safeSeq[count++] = p;
+                    finish[p] = true;
+                    found = true;
+                }
+            }
+        }
+        if (!found) return false;
+    }
 
-				if(canAllocate) {
-					for(int r = 0; r<R; r++) {
-						work[r] += alloc[p][r];
-					}
-					safeSeq[count++] = p;
-					finish[p] = true;
-					found = true;
-				}
-			}
-		}
-		if(!found) return false;
-	}
+    printf("\nSafe Sequence: ");
+    for (int i = 0; i < P; i++) {
+        printf("P%d ", safeSeq[i]);
+    }
+    printf("\n");
 
-	printf("Safe Sequence: ");
-	for(int i = 0; i<P; i++) {
-		printf("%d ", safeSeq[i]);	
-	}
-	printf("\n");
-
-	return true;
+    return true;
 }
 
 int main() {
+    int noProcesses, noResources;
 
-	int noProcesses, noResources;
-	printf("Enter the number of processes: ");
-	scanf("%d", &noProcesses);
-	printf("Enter the number of resources: ");
-	scanf("%d", &noResources);
+    printf("Enter the number of processes: ");
+    scanf("%d", &noProcesses);
+    printf("Enter the number of resources: ");
+    scanf("%d", &noResources);
 
-	int available[noResources];
+    int available[noResources];
+    int max[noProcesses][noResources];
+    int alloc[noProcesses][noResources];
 
-	int max[noProcesses][noResources];
-	
-	int alloc[noProcesses][noResources];
+    printf("Enter the available resources (R1 R2 R3 ...): ");
+    for (int i = 0; i < noResources; i++) {
+        scanf("%d", &available[i]);
+    }
 
-	printf("Enter the available resources: ");
-	for (int i = 0; i < noResources; i++) {
-		scanf("%d", &available[i]);	
-	}
+    printf("\nEnter the maximum resources required by each process:\n");
+    for (int i = 0; i < noProcesses; i++) {
+        printf("Process %d (R1 R2 R3 ...): ", i);
+        for (int j = 0; j < noResources; j++) {
+            scanf("%d", &max[i][j]);
+        }
+    }
 
-	printf("Enter the maximum resources: ");
-	for (int i = 0; i < noProcesses; i++) {
-		for (int j = 0; j < noResources; j++) {	
-			scanf("%d", &max[i][j]);
-		}
-	}
+    printf("\nEnter the allocated resources for each process:\n");
+    for (int i = 0; i < noProcesses; i++) {
+        printf("Process %d (R1 R2 R3 ...): ", i);
+        for (int j = 0; j < noResources; j++) {
+            scanf("%d", &alloc[i][j]);
+        }
+    }
 
-	printf("Enter the allocated resources: ");
-	for (int i = 0; i < noProcesses; i++) {
-		for (int j = 0; j < noResources; j++) {	
-			scanf("%d", &alloc[i][j]);
-		}
-	}
+    if (isSafe(noProcesses, noResources, alloc, max, available)) {
+        printf("\nThe system is in a safe state.\n");
+    } else {
+        printf("\nThe system is not in a safe state.\n");
+    }
 
-	if(isSafe(noProcesses, noResources, alloc, max, available)) {
-		printf("The system is not in safe state");
-	}
-	else {
-		printf("The system is in safe state");	
-	}
-
-	return 0;
+    return 0;
 }
