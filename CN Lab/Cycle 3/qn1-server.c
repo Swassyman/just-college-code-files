5685cd0b831ebf031b5783a865f45779bb7557b4
Swassyman <swassyman2005@gmail.com>
2025-03-27 22:06:58 +0530

    fixed qn1

--- CN Lab/Cycle 3/qn1-server.c
+++ CN Lab/Cycle 3/qn1-server.c
@@ -8,13 +8,11 @@
 #define BUFFER_SIZE 1024
 
 void calculateStats(int arr[], int n, int *max, int *min, float *avg) {
-    *max = arr[0];
-    *min = arr[0];
+    *max = *min = arr[0];
     *avg = 0;
-
-    for(int i = 0; i<n; i++) {
-        if(arr[i] > *max) *max = arr[i];
-        if(arr[i] < *min) *min = arr[i];
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > *max) *max = arr[i];
+        if (arr[i] < *min) *min = arr[i];
         *avg += arr[i];
     }
     *avg /= n;
@@ -22,41 +20,36 @@ void calculateStats(int arr[], int n, int *max, int *min, float *avg) {
 
 int main() {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    int buffer[BUFFER_SIZE];
 
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    struct sockaddr_in server_address, client_address;
+    socklen_t addrlen = sizeof(client_address);
+    
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(PORT);
 
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-
+    bind(server_fd, (struct sockaddr *)&server_address, sizeof(server_address));
     listen(server_fd, 4);
 
-    printf("Server listening\n");
+    while (1) {
+        int client_fd = accept(server_fd, (struct sockaddr *)&client_address, &addrlen);
 
-    while(1) {
-        int client_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+        char buffer[BUFFER_SIZE];
+        recv(client_fd, buffer, BUFFER_SIZE, 0);
 
-        int buffer[BUFFER_SIZE];
-        read(client_fd, buffer, BUFFER_SIZE);
-        
+        int n;
+        memcpy(&n, buffer, sizeof(int));
 
-        printf("%d", buffer[0]);
-        int n = buffer[0];
         int arr[n];
+        memcpy(arr, buffer + sizeof(int), n * sizeof(int));
 
-        for(int i = 1; i < n + 1; i++) {
-            arr[i - 1] = buffer[i];
-        }
         int max, min;
         float avg;
         calculateStats(arr, n, &max, &min, &avg);
 
         char response[BUFFER_SIZE];
         snprintf(response, BUFFER_SIZE, "Max: %d, Min: %d, Avg: %.2f", max, min, avg);
-        send(client_fd, response, BUFFER_SIZE, 0);
+        send(client_fd, response, strlen(response), 0);
 
         close(client_fd);
     }
@@ -64,4 +57,3 @@ int main() {
     close(server_fd);
     return 0;
 }
-
