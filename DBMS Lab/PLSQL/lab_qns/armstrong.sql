428bfb75bbd7456b77fb7cbf678280018af25a9a
Swassy <swassyman2005@gmail.com>
2025-04-08 11:22:46 +0530

    solved qn with function

--- DBMS Lab/PLSQL/lab_qns/armstrong.sql
+++ DBMS Lab/PLSQL/lab_qns/armstrong.sql
@@ -1,23 +1,29 @@
+CREATE OR REPLACE FUNCTION ARMSTRONG(N IN NUMBER)
+RETURN NUMBER IS
+    digit NUMBER;
+    temp NUMBER := N;
+    result NUMBER := 0;
+BEGIN
+    WHILE temp > 0 LOOP
+        digit := MOD(temp, 10);
+        result := result + POWER(digit, 3);
+        temp := TRUNC(temp / 10);
+    END LOOP;
+
+    RETURN result;
+END;
+/
+
 SET SERVEROUTPUT ON;
 DECLARE
-	num NUMBER;
-	digit NUMBER;
-	result NUMBER;
-	temp NUMBER;
+    num NUMBER := 153;
+    result NUMBER;
 BEGIN
-	num := 153;
-	temp := num;
-	result := 0;
-
-	WHILE temp > 0 LOOP
-		digit := MOD(temp, 10);
-		result := result + (digit*digit*digit);
-		temp := TRUNC(temp/10);
-	END LOOP;
-
-	IF num=result THEN
-		DBMS_OUTPUT.PUT_LINE('Armstrong');
-	ELSE 
-		DBMS_OUTPUT.PUT_LINE('Not Armstrong');
-	END IF;
+    result := ARMSTRONG(num);
+    IF num = result THEN
+        DBMS_OUTPUT.PUT_LINE('Armstrong');
+    ELSE 
+        DBMS_OUTPUT.PUT_LINE('Not Armstrong');
+    END IF;
 END;
+/
