d658ed52f196435e9e17fa9629a4c8dd2f182cf3
Swassy <84135882+Swassyman@users.noreply.github.com>
2025-05-02 01:28:09 +0530

    Update qn3-client.c

--- CN Lab/Cycle 3/qn3-client.c
+++ CN Lab/Cycle 3/qn3-client.c
@@ -1,45 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/select.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
-
-#define BUFFER_SIZE 1024
 
 int main() {
-    int client_fd;
+    int client = socket(AF_INET, SOCK_STREAM, 0);
+
     struct sockaddr_in server_addr;
-    char buffer[BUFFER_SIZE];
-    fd_set read_fds;
-
-    client_fd = socket(AF_INET, SOCK_STREAM, 0);
-
+    server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(12345);
 
-    connect(client_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    connect(client, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
-    printf("Connected to the server. Start chatting!\n");
+    char message[1024];
 
     while (1) {
-        FD_ZERO(&read_fds);
-        FD_SET(STDIN_FILENO, &read_fds);
-        FD_SET(client_fd, &read_fds);
+        fd_set fds;
+        FD_ZERO(&fds);
+        FD_SET(STDIN_FILENO, &fds);
+        FD_SET(client, &fds);
+        int max_fd = (STDIN_FILENO > client) ? STDIN_FILENO : client;
 
-        select(client_fd + 1, &read_fds, NULL, NULL, NULL);
+        select(max_fd + 1, &fds, NULL, NULL, NULL);
 
-        if (FD_ISSET(STDIN_FILENO, &read_fds)) {
-            fgets(buffer, BUFFER_SIZE, stdin);
-            send(client_fd, buffer, strlen(buffer), 0);
+        if (FD_ISSET(STDIN_FILENO, &fds)) {
+            fgets(message, sizeof(message), stdin);
+            message[strcspn(message, "\n")] = 0;
+            send(client, message, strlen(message) + 1, 0);
         }
 
-        if (FD_ISSET(client_fd, &read_fds)) {
-            int bytes_received = recv(client_fd, buffer, BUFFER_SIZE, 0);
-            buffer[bytes_received] = '\0';
-            printf("%s", buffer);
+        if (FD_ISSET(client, &fds)) {
+            int len = recv(client, message, sizeof(message), 0);
+            if (len <= 0) {
+                printf("Disconnected!\n");
+                break;
+            }
+            printf("%s\n", message);
         }
     }
-    
-    close(client_fd);
+
+    close(client);
     return 0;
 }
