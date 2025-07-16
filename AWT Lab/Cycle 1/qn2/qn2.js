fa99850d676c0a3099b85a9959f6f6278ecae6d5
Swassyman <swassyman2005@gmail.com>
2025-07-16 10:57:05 +0530

    Cycle 1 Questions 1-3 done

---
+++ AWT Lab/Cycle 1/qn2/qn2.js
@@ -0,0 +1,11 @@
+class Student {
+  constructor(name, rollNo, mark1, mark2) {
+    (this.name = name),
+      (this.rollNo = rollNo),
+      (this.mark1 = mark1),
+      (this.mark2 = mark2);
+  }
+  getAverage() {
+    document.writeln(this.name + " got " + (this.mark2 + this.mark1) / 2 + "% for her final exam.");
+  }
+}
