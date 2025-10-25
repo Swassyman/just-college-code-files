ba0d1a0a435823e2a6c5036e2defbd2d864b8a4a
Swassyman <swassyman2005@gmail.com>
2025-10-25 11:05:12 +0530

    fix: oops accidentally pushed node modules

--- AWT Lab/classroom things/class-app/src/App.js
+++ AWT Lab/classroom things/class-app/src/App.js
@@ -4,8 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      Hello
     </div>
   );
 }
 
+function ButtonComponent() {
+  const[isRed, setisRed] = useState(false);
+
+  const style = {
+    color: isRed ? "red": "black";
+  }
+
+
+}
 export default App;
